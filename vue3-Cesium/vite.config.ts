745483037eabf4b732f8a1b10a9315b6f5aca7a5
wanglei <wangl1128@chinaunicom.cn>
2025-01-21 15:40:20 +0800

    init

--- vue3-Cesium/vite.config.ts
+++ vue3-Cesium/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import cesium from 'vite-plugin-cesium'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,10 +12,14 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    cesium()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  optimizeDeps: {
+    include: ['cesium'],
   },
 })
